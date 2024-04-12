f0bc23c700f4ec1e6427c44d5148aa18cd4db3bf
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-04-12 15:17:16 +0800

    :sparkles: inherit class method works for all

--- main.cpp
+++ main.cpp
@@ -44,6 +44,56 @@ namespace TestNamespace {
     }
 }
 
+struct ClassA {
+    char * func0(int) {
+        std::string str = "Non mocked 0";
+        char* cstr = new char[str.length() + 1];  // +1 for the null terminator
+        std::strcpy(cstr, str.c_str());
+        return cstr;
+    }
+    int a;
+};
+
+struct ClassB : ClassA {
+    char * func0(int) {
+        std::string str = "Non mocked 1";
+        char* cstr = new char[str.length() + 1];  // +1 for the null terminator
+        std::strcpy(cstr, str.c_str());
+        return cstr;
+    }
+    int b;
+};
+
+struct ClassC : ClassB {
+    char * func0(int) {
+        std::string str = "Non mocked 2";
+        char* cstr = new char[str.length() + 1];  // +1 for the null terminator
+        std::strcpy(cstr, str.c_str());
+        return cstr;
+    }
+    int c;
+};
+
+struct ClassD {
+    char * func0(int) {
+        std::string str = "Non mocked 3";
+        char* cstr = new char[str.length() + 1];  // +1 for the null terminator
+        std::strcpy(cstr, str.c_str());
+        return cstr;
+    }
+    int d;
+};
+
+struct ClassE : ClassC, ClassD {
+    char * func0(int) {
+        std::string str = "Non mocked 4";
+        char* cstr = new char[str.length() + 1];  // +1 for the null terminator
+        std::strcpy(cstr, str.c_str());
+        return cstr;
+    }
+    int e;
+};
+
 int main() {
     MOCKER(func);
     std::cout << "global method - func return: " << func(1) << std::endl;
@@ -62,6 +112,17 @@ int main() {
 
     MOCKER(strerror);
     std::cout << "lib method - strerror return: " << strerror(1) << std::endl;
+
+    MOCKER(&ClassA::func0);
+    std::cout << "class method - ClassA::func0 return: " << ClassA().func0(1) << std::endl;
+    MOCKER(&ClassB::func0);
+    std::cout << "class method - ClassB::func0 return: " << ClassB().func0(1) << std::endl;
+    MOCKER(&ClassC::func0);
+    std::cout << "class method - ClassC::func0 return: " << ClassC().func0(1) << std::endl;
+    MOCKER(&ClassD::func0);
+    std::cout << "class method - ClassD::func0 return: " << ClassD().func0(1) << std::endl;
+    MOCKER(&ClassE::func0);
+    std::cout << "class method - ClassE::func0 return: " << ClassE().func0(1) << std::endl;
     return 0;
 }
 
